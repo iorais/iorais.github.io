9bc72c0e24c098694b6043442d2a00e23325af70
Isaac Orais <isaacorais2002@gmail.com>
2022-11-04 23:43:43 -0700

    remove not needed

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,10 +5,8 @@ export default function Navbar() {
         <div>
             <nav className = "nav">
                 <div class="logo" />
-                <ul>
                     <h2> <a href = "/about">About</a> </h2>
                     <h2> <a href = "/projects">Projects</a> </h2>
-                </ul>
             </nav>
         </div>  
     )
